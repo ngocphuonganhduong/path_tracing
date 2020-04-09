ddd1f42febd45e18c562549a4d27e73bca99da6c
Ngoc Phuong Anh DUONG <ngocphuonganh.duong@epita.fr>
2020-04-09 14:16:41 +0200

    scene: fix main3

--- src/main3.cpp
+++ src/main3.cpp
@@ -5,14 +5,14 @@
 using namespace pathtracing;
 
 int main(int argc, char **argv) {
-    
+
     objl::Loader loader;
     TriVector t;
     TriVector t2;
 
     loader.load_object("../src/objects/assets/box.obj", t);
     loader.load_object("../src/objects/assets/tall_box.obj", t2);
-    
+
     //color
     Vector3 red(1, 0.4, 0.4);
     Vector3 green(0.4, 1, 0.4);
@@ -24,7 +24,7 @@ int main(int argc, char **argv) {
     Vector3 cyan(0, 1, 1);
     Vector3 purple(0.7, 0, 1);
     Vector3 wall_color(0.95, 0.92, 0.97);
-    double half_l = 4.2;
+    double half_l = 4.6;
     double distance = 16;
 
 
@@ -36,19 +36,20 @@ int main(int argc, char **argv) {
     double kr = 0.5;
     double d = 1;
 
-    shared_mat mat = std::make_shared<Material>(ka, white * 0.5 , white, 32);
+    shared_mat mat = std::make_shared<Material>(ka, white, white, 32);
     // the small box
-    auto o1 = std::make_shared<TriangleMesh>(Vector3(2, 11, -3), std::make_shared<DiffuseBSDF>(mat), t, Vector3(1.2,1.2,1.2), Vector3(0,0,-30));
+    auto o1 = std::make_shared<TriangleMesh>(Vector3(2, 11, -3.4), std::make_shared<DiffuseBSDF>(mat), t,
+                                             Vector3(1.2, 1.2, 1.2), Vector3(0, 0, -30));
 
-    shared_mat mat2 = std::make_shared<Material>(ka, white, white, 32);
     // the tall box
-    auto o2 = std::make_shared<TriangleMesh>(Vector3(-2, 12.5, -2), std::make_shared<DiffuseBSDF>(mat2), t2, Vector3(1.2,1.2,1.2), Vector3(0,0,30));
+    auto o2 = std::make_shared<TriangleMesh>(Vector3(-2, 12.5, -2.6), std::make_shared<DiffuseBSDF>(mat), t2,
+                                             Vector3(1.2, 1.2, 1.2), Vector3(0, 0, 30));
 
     // the sphere
-    auto o3 = std::make_shared<Sphere>(Vector3(-2, 12.5, 1.4), std::make_shared<MirrorBSDF>(mat2), 1.0);
+    auto o3 = std::make_shared<Sphere>(Vector3(-2, 12.5, 0.94), std::make_shared<MirrorBSDF>(mat), 1.0);
 
     shared_mat mat_light = std::make_shared<Material>(ka, white, ks, ns);
-    mat_light->set_emission(white * 6, 0.2, 0, 1);
+    mat_light->set_emission(white * 5, 0.2, 0, 1);
 
 
     shared_obj area_light = std::make_shared<Square>(Vector3(0, distance - 4, half_l - 0.1),
@@ -63,7 +64,7 @@ int main(int argc, char **argv) {
                                                 std::make_shared<PhongBSDF>(mat_f),
                                                 Vector3(0, 0, 1),
                                                 Vector3(0, 1, 0),
-                                                half_l);
+                                                half_l * 4);
 
 
     shared_bsdf wall_bsdf = std::make_shared<DiffuseBSDF>(std::make_shared<Material>(ka, white, ks, ns));
@@ -71,27 +72,33 @@ int main(int argc, char **argv) {
     shared_bsdf left_wall_bsdf = std::make_shared<DiffuseBSDF>(std::make_shared<Material>(ka, red, ks, ns));
     shared_bsdf right_wall_bsdf = std::make_shared<DiffuseBSDF>(std::make_shared<Material>(ka, blue, ks, ns));
 
+//    shared_bsdf behind_bsdf = std::make_shared<DiffuseBSDF>(std::make_shared<Material>(ka, white, ks, ns));
+
 
     shared_obj roof = std::make_shared<Square>(Vector3(0, distance - half_l, half_l),
                                                root_bsdf, Vector3(0, 0, -1),
                                                Vector3(0, 1, 0),
-                                               half_l);
+                                               half_l *4);
 
     shared_obj front_w = std::make_shared<Square>(Vector3(0, distance, 0),
                                                   wall_bsdf, Vector3(0, -1, 0),
                                                   Vector3(0, 0, 1),
-                                                  half_l);
+                                                  half_l * 4);
+    shared_obj behind_w = std::make_shared<Square>(Vector3(0, -0.01, 0),
+                                                  wall_bsdf, Vector3(0, 1, 0),
+                                                  Vector3(0, 0, 1),
+                                                  half_l * 1000);
 
 
     shared_obj left_w = std::make_shared<Square>(Vector3(-half_l, distance - half_l, 0),
                                                  left_wall_bsdf, Vector3(1, 0, 0),
                                                  Vector3(0, 0, 1),
-                                                 half_l);
+                                                 half_l * 4) ;
 
     shared_obj right_w = std::make_shared<Square>(Vector3(half_l, distance - half_l, 0),
                                                   right_wall_bsdf, Vector3(-1, 0, 0),
                                                   Vector3(0, 0, 1),
-                                                  half_l);
+                                                  half_l * 4);
     Camera cam(Vector3(0, 0, 0), Vector3(0, 1, 0), Vector3(0, 0, 1), 60);
     Scene scene(700, 700, cam, Vector3(0.2, 0.2, 0.2));
 
@@ -100,6 +107,7 @@ int main(int argc, char **argv) {
     scene.add_object(o3);
     scene.add_object(area_light);
     scene.add_object(front_w);
+    scene.add_object(behind_w);
     scene.add_object(roof);
     scene.add_object(floor);
     scene.add_object(left_w);
